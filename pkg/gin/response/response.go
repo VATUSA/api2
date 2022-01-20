4e1c18c1b5e831b2552085ef9cf0822df90876c9
Daniel Hawton <daniel@hawton.org>
2022-01-19 19:27:30 -0700

    add gin dep, start response pkg

---
+++ pkg/gin/response/response.go
@@ -0,0 +1,47 @@
+package response
+
+import (
+	"encoding/xml"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type R struct {
+	XMLName xml.Name    `xml:"response"`
+	Status  string      `xml:"status" json:"status"`
+	Data    interface{} `xml:"data" json:"data"`
+}
+
+func RespondMessage(c *gin.Context, status int, message string) {
+	Respond(c, status, struct {
+		Message string `json:"message" xml:"message"`
+	}{message})
+}
+
+func RespondBlank(c *gin.Context, status int) {
+	c.Status(status)
+	c.Abort()
+}
+
+func RespondError(c *gin.Context, status int, message string) {
+	RespondMessage(c, status, message)
+}
+
+func Respond(c *gin.Context, status int, data interface{}) {
+	ret := R{}
+	ret.Status = http.StatusText(status)
+	ret.Data = data
+
+	if acceptYaml(c.GetHeader("Accept")) {
+		c.YAML(status, ret)
+	} else if c.GetHeader("Accept") == "application/xml" {
+		c.XML(status, ret)
+	} else {
+		c.JSON(status, ret)
+	}
+}
+
+func acceptYaml(accept string) bool {
+	return accept == "application/yaml" || accept == "text/yaml" || accept == "application/x-yaml"
+}
